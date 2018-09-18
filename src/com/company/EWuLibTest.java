4bd596f78d0959ace36b11bc66e71829d783aa51
Edmund Wu <fangkazuto@gmail.com>
2018-09-17 22:00:40 -0400

    Include main and test files

---
+++ src/com/company/EWuLibTest.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EWuLibTest {
+
+    @org.junit.jupiter.api.Test
+    void isPalindrome() {
+        // These are palindromes
+        assert(EWuLib.isPalindrome("racecar"));
+        assert(EWuLib.isPalindrome("lkjjkl"));
+        // This isn't
+        assert(EWuLib.isPalindrome("hi") == false);
+    }
+
+    @org.junit.jupiter.api.Test
+    void dateStr() {
+        // Make sure '/' doesn't exist anymore
+        assert(EWuLib.dateStr("09/23/1239").indexOf('/') == -1);
+        // Nothing should change here
+        assert(EWuLib.dateStr("12324234").equals("12324234"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void cutOut() {
+        String mainStr = "abc123";
+        String mainStr2 = "abc23";
+        String subStr = "c12";
+
+        // Make sure the new string has a smaller length
+        assert(EWuLib.cutOut(mainStr, subStr).length() < mainStr.length());
+        // Make sure this combination doesn't change anything
+        assert(EWuLib.cutOut(mainStr2, subStr).equals(mainStr2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fooBarBaz() {
+        // No actual way to check stdout, so just check it runs properly here
+        EWuLib.fooBarBaz(34);
+    }
+
+    @org.junit.jupiter.api.Test
+    void vigCipher() {
+        // Wikipedia example
+        assert(EWuLib.vigCipher("ATTACKATDAWN", "LEMONLEMONLE").equals("LXFOPVEFRNHR"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void stringUnion() {
+        // Default example
+        assert(EWuLib.stringUnion("hello", "hola", "nihao") == 4);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isFibonacci() {
+        // All numbers here should work
+        assert(EWuLib.isFibonacci(EWuLib.fibonacci(12)));
+        assert(EWuLib.isFibonacci(EWuLib.fibonacci(42)));
+        // Just a random number
+        assert(EWuLib.isFibonacci(100) == false);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicationTable() {
+        // No actual way to check stdout, so just check it runs properly here
+        EWuLib.multiplicationTable(5, 5);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumUpTo() {
+        // Checking sum
+        assert(EWuLib.sumUpTo(3) == 3);
+        assert(EWuLib.sumUpTo(15) == 105);
+        // Obvious logic
+        assert(EWuLib.sumUpTo(100) > 100);
+        assert(EWuLib.sumUpTo(23) > 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void quadSolver() {
+        // Checking answers
+        assert(EWuLib.quadSolver(1, 2, -24).equals("4.0, -6.0"));
+        assert(EWuLib.quadSolver(3.4, 5.6, 2.4).equals("Imaginary numbers"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void leastCommonMultiple() {
+        // Just random stuff at this point
+        assert(EWuLib.leastCommonMultiple(1, 2, 3) == 6);
+        assert(EWuLib.leastCommonMultiple(4, 5, 6) >= Math.max(4, Math.max(5, 6)));
+        assert(EWuLib.leastCommonMultiple(10, 20, 30) == 60);
+    }
+}
---
+++ src/com/company/testFile.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import static com.company.EWuLib.*;
+
+public class testFile {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(dateStr("09/23/1239"));
+        System.out.println(cutOut("abc123", "c12"));
+        fooBarBaz(34);
+
+        System.out.println(stringUnion("hello", "hola", "nihao"));
+        System.out.println(vigCipher("attackatdawn", "lemon"));
+
+        System.out.println(isFibonacci(55));
+        multiplicationTable(5, 5);
+        System.out.println(sumUpTo(15));
+
+        System.out.println(quadSolver(1, 2, -24));
+        System.out.println(leastCommonMultiple(4, 2, 3));
+    }
+}
